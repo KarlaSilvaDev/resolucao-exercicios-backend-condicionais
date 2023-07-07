58a9b9ceb2d835599a4c42e20de656414d2ca187
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-07 15:03:54 -0300

    Ex03-Dominó

--- 03/index.js
+++ 03/index.js
@@ -1,4 +1,6 @@
-const ladoA = 3;
-const ladoB = 3;
+const ladoA = 2;
+const ladoB = 4;
 
 //seu código aqui
+console.log(`Lado A: ${ladoA} \nLado B: ${ladoB} \nEssa pedra é uma bucha?`);
+console.log(ladoA == ladoB ? "SIM" : "NÃO");
