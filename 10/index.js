64257f8514ea4b7aa3c6192dbbcc8de6a4abc332
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-07 23:45:48 -0300

    Ex10 - Identificando Caracteres

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,17 @@
 const caractere = "E";
+
+if (caractere.length == 1){
+    if (/[aeiou]/.test(caractere)){
+        console.log("Vogal minúscula");
+    }else if (/[AEIOU]/.test(caractere)){
+        console.log("Vogal maiúscula");
+    }else if (/[a-z]/.test(caractere) || /[A-Z]/.test(caractere)){
+        console.log("Consoante");
+    }else if (/[0123956789]/.test(caractere)){
+        console.log("É um número");
+    }else{
+        console.log("Não é um número ou letra.");
+    }
+}else{
+    console.log("Entrada inválida. O programa avalia apenas UM caractere");
+}
