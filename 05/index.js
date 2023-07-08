60ac9c1b98117c86e4a31b7c84b043b2d702a0dd
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-07 21:45:31 -0300

    Ex05 - Peneira de Vôlei

--- 05/index.js
+++ 05/index.js
@@ -1,3 +1,12 @@
-const alturaEmCm = 185;
+const alturaEmCm = 180;
 
 //seu código aqui
+if (isNaN(alturaEmCm)){
+    console.log("Altura inválida!");
+}else{
+    if (alturaEmCm>=180){
+        console.log("APROVADO");
+    }else{
+        console.log("REPROVADO");
+    }   
+}
