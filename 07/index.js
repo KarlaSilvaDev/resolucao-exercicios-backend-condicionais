a318317761ed3a72ef9b9624f97b45cba3d96f2a
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-07 22:47:35 -0300

    Ex07 - Isenção de Impostos

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,13 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 3_000_000; //em centavos
 
-//seu código aqui
+if ((aposentada) || (portadoraDeDoenca)){
+    console.log("ISENTA");
+}else{
+    if (totalDeRendimentos > 2_855_970){ //transformei o valor em centavos (1 real = 100 centavos)
+        console.log("PEGA LEAO");
+    }else{
+        console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+    }
+}
