3b72b04b961a6a2ab96708ab2cfdf40450ed1c17
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-09 00:01:13 -0300

    Ex15 - Nome para Exibição

--- 15/index.js
+++ 15/index.js
@@ -1,4 +1,15 @@
 const primeiroNome = "Mario";
 const sobrenome = "";
-const apelido = "";
+const apelido = "Bros";
 
+if (primeiroNome){ //resultará em false se primeiroNome = ""
+    if (apelido){ //resultará false se apelido = ""
+        console.log(apelido);
+    }else if (sobrenome){ 
+        console.log(primeiroNome+" "+sobrenome);
+    }else{
+        console.log(primeiroNome);
+    }  
+}else{
+    console.log("O preenchimento do primeiro nome é obrigatório.");
+}
