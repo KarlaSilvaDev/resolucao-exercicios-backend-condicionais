2026ae86f4eeaf68cf81dd63f8c3e1c6aed58762
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-09 00:41:04 -0300

    Ex01 - Correção das impressões na tela

--- 01/index.js
+++ 01/index.js
@@ -1,28 +1,27 @@
-const jogada1 = "tesoura";
-const jogada2 = "pedra";
+const jogada1 = "pedra";
+const jogada2 = "tesoura";
 
-console.log(`Jogador 1: ${jogada1} \nJogador 2: ${jogada2}`);
 if (jogada1 == "pedra" || jogada1 == "papel" || jogada1 == "tesoura"){  //VALIDAÇÃO JOGADA1
     if (jogada2 == "pedra" || jogada2 == "papel" || jogada2 == "tesoura"){ //VALIDAÇÃO JOGADA2
         if (jogada1 == jogada2){  //CASOS DE EMPATE
-            console.log("Resultado: EMPATE!");  
-        }else if (jogada1 == "pedra"){    //COMBINAÇÕES COM JOGADOR 1 ESCOLHENDO PEDRA 
+            console.log("empate");  
+        }else if (jogada1 == "pedra") {    //COMBINAÇÕES COM JOGADOR 1 ESCOLHENDO PEDRA 
             if (jogada2 == "papel"){   
-                console.log("Resultado: JOGADOR 2 VENCEU!");
+                console.log("papel");
             }else{   //COMO NÃO DEU EMPATE, JOGADOR 2 = TESOURA
-                console.log("Resultado: JOGADOR 1 VENCEU!");
+                console.log("pedra");
             }  
         }else if (jogada1 == "papel"){   //COMBINAÇÕES COM JOGADOR 1 ESCOLHENDO PAPEL
             if (jogada2 == "pedra"){    
-                console.log("Resultado: JOGADOR 1 VENCEU!");
+                console.log("papel");
             }else{   //COMO NÃO DEU EMPATE, JOGADOR 2 = TESOURA
-                console.log("Resultado: JOGADOR 2 VENCEU!");
+                console.log("tesoura");
             }  
         }else if (jogada1 == "tesoura"){ //COMBINAÇÕES COM JOGADOR 1 ESCOLHENDO TESOURA
             if (jogada2 == "papel"){    
-                console.log("Resultado: JOGADOR 1 VENCEU!");
+                console.log("tesoura");
             }else{   //COMO NÃO DEU EMPATE, JOGADOR 2 = PEDRA
-                console.log("Resultado: JOGADOR 2 VENCEU!");
+                console.log("pedra");       
             }  
         }
     }else{
