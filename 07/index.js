17a59a7fea0ece6fe2fcbeb6ecfbb6069978b13f
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-09 00:52:10 -0300

    Ex07 - Inserção da constante rendimentoLimite

--- 07/index.js
+++ 07/index.js
@@ -2,10 +2,12 @@ const aposentada = false;
 const portadoraDeDoenca = false;
 const totalDeRendimentos = 3_000_000; //em centavos
 
+const rendimentoLimite = 2_855_970; //em centavos
+
 if ((aposentada) || (portadoraDeDoenca)){
     console.log("ISENTA");
 }else{
-    if (totalDeRendimentos > 2_855_970){ //transformei o valor em centavos (1 real = 100 centavos)
+    if (totalDeRendimentos > rendimentoLimite){ //transformei o valor em centavos (1 real = 100 centavos)
         console.log("PEGA LEAO");
     }else{
         console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
